3adb2d896247a3ed2f1522a2e248c545d00196dc
Mathias Erik Nord <mathias.erik.nord@gmail.com>
2025-06-26 20:26:34 +0200

    feat: :sparkles: Added dynamic status message.
    Added dynamically changing status message based on future feedback from backend.

--- extension/popup/popup_script.js
+++ extension/popup/popup_script.js
@@ -1,9 +1,25 @@
-
 /**
- * Event listener for the settings button in the popup.
- * When clicked, it opens the options page of the extension.
+ * Listener that loads when the DOM is loading/loaded.
  */
 document.addEventListener("DOMContentLoaded", function () {
   const settingsButton = document.querySelector(".settings-button");
-  settingsButton.addEventListener("click", chrome.runtime.openOptionsPage());
+  settingsButton.addEventListener("click", function () {
+    chrome.runtime.openOptionsPage();
+  });
+
+  changeStatus(true);
 });
+
+/**
+ * Function that changes the status message based on boolean values.
+ *
+ * @param {boolean} bool - True if safe, false in other cases.
+ */
+function changeStatus(bool) {
+  const status = document.getElementById("status-message");
+  if (bool) {
+    status.innerHTML = "✅ Site is safe";
+  } else {
+    status.innerHTML = "⛔️ Site is unsafe";
+  }
+}
